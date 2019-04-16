b81185b150ec70ec27b8b047d8a95d6f2467645e
Urbanz91 <turbino4ka91@gmail.com>
2019-04-17 01:53:09 +0300

    homework_01

---
+++ src/UrbanyukAleksandr/homework_01.ts
@@ -0,0 +1,50 @@
+// 1)
+
+function isInArray2(arr: unknown[], ...args: unknown[]): boolean {
+  return args.every((val: unknown) => arr.includes(val));
+}
+
+// 2)
+
+type ns = string | number;
+function summator(...args: ns[]): ns {
+  return args.reduce((acc: number, currentVal: ns) => {
+    if (typeof currentVal === 'string') {
+      currentVal = Number(currentVal);
+    }
+    return acc + currentVal;
+  }, 0);
+}
+
+//3)
+
+function getUnique(...args: unknown[]): unknown[] {
+  const newSet = new Set();
+  args.map(val => {
+    newSet.add(val);
+  });
+  const arr: unknown[] = [];
+  newSet.forEach((val) => {
+    arr.push(val);
+  });
+  return arr;
+}
+
+// 4)
+
+function toMatrix(data: unknown[], rowSize: number): unknown[] {
+  const matrix: unknown[] = [];
+  const amountRow: number = Math.ceil(data.length / rowSize);
+  for (let i = 0; i < data.length; i++ ) {
+    if (amountRow === matrix.length) {
+      break;
+    }
+    if (i !== 0) {
+      matrix.push(data.slice(rowSize * i, rowSize * (i + 1)));
+      continue;
+    }
+    matrix.push(data.slice(0, rowSize));
+    continue;
+  }
+  return matrix;
+}
